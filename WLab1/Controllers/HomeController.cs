805b08163f09f9e6d720f5c2d7a8e1f10ad47366
Handowl <elisey.trushkov@gmail.com>
2020-05-03 22:55:26 +0700

    Second try

---
+++ WLab1/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WLab1.Models;
+
+namespace WLab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Web lab by Elisey Trushkov.";
+            ViewData["Message1"] = "discord - handowl#3724.";
+            ViewData["Message2"] = "vk - id391491070 ";
+
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
